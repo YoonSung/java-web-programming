85ec825659516d6bb6ef1f448645f7baa669b3e4
Yoonsung <lvev9925@naver.com>
2014-09-26 01:27:26 +0900

    테스트 코드를 AnnotationConfigApplicationContext를 활용하도록 수정함.

--- qna/src/test/java/next/dao/QuestionDaoTest.java
+++ qna/src/test/java/next/dao/QuestionDaoTest.java
@@ -4,30 +4,38 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import next.config.Config;
 import next.model.Question;
 import next.support.db.ConnectionManager;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 public class QuestionDaoTest {
+	
+	QuestionDao questionDao;
+	
 	@Before
 	public void setup() {
 		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
 		populator.addScript(new ClassPathResource("qna.sql"));
 		DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+		
+		try (AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Config.class)) {
+			questionDao = ac.getBean(QuestionDao.class);
+		}
 	}
 
 	@Test
 	public void crud() throws Exception {
 		Question expected = new Question("자바지기", "title", "contents");
-		QuestionDao dut = new QuestionDao();
-		dut.insert(expected);
+		questionDao.insert(expected);
 		
-		List<Question> questions = dut.findAll();
+		List<Question> questions = questionDao.findAll();
 		assertTrue(questions.size() > 0);
 	}
 }
